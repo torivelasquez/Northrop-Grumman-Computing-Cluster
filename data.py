03e8396fcde760cb45bca12f979571ca65ba1ba1
Dmitriy Tarasov <dmta9796@colorado.edu>
2017-12-11 11:48:23 -0700

    in progress training

--- data.py
+++ data.py
@@ -2,16 +2,17 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+def transformation():
+	return transforms.Compose(
+	    [transforms.ToTensor(),
+	     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 
-def traindata():
+def traindata(transform):
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
     return torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
 
 
-def testdata():
+def testdata(transform):
     testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
     return torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
