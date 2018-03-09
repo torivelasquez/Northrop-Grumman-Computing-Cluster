78cee2c1d857829a9f51fcb715dd63e06e1aff0b
scottlyoung <scottslyyoung@gmail.com>
2018-03-08 19:46:49 -0700

    Updated split to split each class evenly.

--- data_spliter.py
+++ data_spliter.py
@@ -3,35 +3,43 @@ import math
 
 
 def data_spliter(in_file_name, out_file_names, fractions):
-    if not math.isclose(sum(fractions), 1, rel_tol = 1e-5):
+    if not math.isclose(sum(fractions), 1, rel_tol=1e-5):
         return 1
     num_in_lines = 0
+    class_counts = {}
     with open(in_file_name, 'r') as in_file:
-        for _ in in_file:
+        for line in in_file:
             num_in_lines += 1
+            line_class = line.split(',')[1]
+            if not line_class in class_counts:
+                 class_counts[line_class] = 0
+            class_counts[line_class] += 1
         in_file.seek(0)
         out_files = []
         num_writes_left = []
         for frac in fractions:
-            num_writes_left.append(math.ceil(frac * num_in_lines))
+            out_file_class_counts = {}
+            for class_name in class_counts:
+                out_file_class_counts[class_name] = math.ceil(frac * class_counts[class_name])
+            num_writes_left.append(out_file_class_counts)
         for out_file_name in out_file_names:
             out_files.append(open(out_file_name, "w"))
         for line in in_file:
             num = random.random()   # generates a pseudo-random number between 0 and 1
             file_classifier_bound = 0
+            line_class = line.split(',')[1]
             for i in range(len(out_files)):
                 file_classifier_bound += fractions[i]
                 if num <= file_classifier_bound:
-                    if num_writes_left[i] != 0:
-                        num_writes_left[i] -= 1
+                    if num_writes_left[i][line_class] != 0:
+                        num_writes_left[i][line_class] -= 1
                         out_files[i].write(line)
                     else:
                         for j in range(len(out_files)):
-                            if num_writes_left[j] != 0:
-                                num_writes_left[j] -= 1
+                            if num_writes_left[j][line_class] != 0:
+                                num_writes_left[j][line_class] -= 1
                                 out_files[j].write(line)
                     break
-
 
         for out_file in out_files:
             out_file.close()
