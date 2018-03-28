970b0b8e85a7904c76b5d2aa2e83924645bc9146
user <dmta9796@colorado.edu>
2018-03-28 13:33:33 -0600

    work in progress adding layers(takes an arguments from layers and adds the layer amount of relu layer as it doesn't change the dimension of tensor)

--- net_algorithms.py
+++ net_algorithms.py
@@ -13,15 +13,17 @@
 #       nn.Linear(a,b) linear tensor transformation from a dimension to b dimension
 #
 #
+import math
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torchvision.models as models
+import runtime_parameters
 alexnet = models.alexnet(pretrained=True)
 resnet = models.resnet18(pretrained=True)
 
 class BaseNet(nn.Module):
-    def __init__(self, output_size):
+    def __init__(self, output_size,params_t):
         super(BaseNet, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
@@ -41,7 +43,7 @@ class BaseNet(nn.Module):
 
 
 class TransferNet(nn.Module):
-    def __init__(self, output_size):
+    def __init__(self, output_size,params_t):
         super(TransferNet, self).__init__()
         self.features = alexnet.features
         self.classifier = nn.Sequential(
@@ -62,7 +64,7 @@ class TransferNet(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self,output_size):
+    def __init__(self,output_size,params_t):
         super(ResNet, self).__init__()
         self.features = nn.Sequential(*list(resnet.children())[:-1])
         self.classifier = nn.Sequential(
@@ -72,17 +74,90 @@ class ResNet(nn.Module):
         )
 
     def forward(self, x):
-        # print(x)
         f = self.features(x)
-        # print(f)
         f = f.view(f.size(0), 512 * 7 * 7) # the size of tensor is 512 * 7 * 7
-        # print(f)
         y = self.classifier(f)
         return y
 
 
+# params = runtime_parameters.Parameters()
+#layers = runtime_parameters.Parameters().get_layers()
+
+
+class LayeredResNet(nn.Module):
+    def __init__(self,output_size,params_t):
+        super(LayeredResNet, self).__init__()
+        self.features = nn.Sequential(*list(resnet.children())[:-1])
+        self.layers = params_t.get_layers()
+        self.layersequence=[]
+        self.make_layers()
+        print(runtime_parameters.Parameters().list())
+        self.classifier = nn.Sequential(
+            *self.layersequence,
+            # [[nn.ReLU(inplace=True) for i in range(layers[j])] for j in range(len(layers))],
+            nn.Linear(512 * 7 * 7, output_size)
+        )
+        print(self.classifier)
+        # self._initialize_weights()
+
+    def forward(self, x):
+        f = self.features(x)
+        f = f.view(f.size(0), 512 * 7 * 7) # the size of tensor is 512 * 7 * 7
+        y = self.classifier(f)
+        return y
+
+    def make_layers(self):
+        for i in range(self.layers[0]):
+            self.layersequence+=[nn.ReLU(inplace=True)] #,nn.Linear(512*7*7,512*7*7)]
+
+
+
+    #  def _initialize_weights(self):
+    #     for m in self.modules():
+    #         if isinstance(m, nn.Conv2d):
+    #             n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+    #             m.weight.data.normal_(0, math.sqrt(2. / n))
+    #             if m.bias is not None:
+    #                 m.bias.data.zero_()
+    #         elif isinstance(m, nn.BatchNorm2d):
+    #             m.weight.data.fill_(1)
+    #             m.bias.data.zero_()
+    #         elif isinstance(m, nn.Linear):
+    #             n = m.weight.size(1)
+    #             m.weight.data.normal_(0, 0.01)
+    #             m.bias.data.zero_()
+
+
+# def added_layers(cfg,batch_norm=False):
+#     layers = []
+#     in_channels = 3
+#     for v in cfg:
+#         if v == 'M':
+#             layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+#         else:
+#             conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+#             if batch_norm:
+#                 layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+#             else:
+#                 layers += [conv2d, nn.ReLU(inplace=True)]
+#             in_channels = v
+#     return nn.Sequential(*layers)
+#
+# cfg = {
+#     'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+#     'B': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+#     'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+#     'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+# }
+#
+# def new_Resnet():
+#     model = LayeredResNet(added_layers(cfg['A']))
+#     return model
+
+
+
 class Net(nn.Module):
-    def __init__(self, output_size):
+    def __init__(self, output_size,params_t):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
@@ -106,7 +181,7 @@ class Net(nn.Module):
 
 
 class GrayNet(nn.Module):
-    def __init__(self, output_size):
+    def __init__(self, output_size,params_t):
         super(GrayNet, self).__init__()
         self.conv1 = nn.Conv2d(1, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
@@ -141,12 +216,12 @@ class MinimalNet(nn.Module):
         return x
 
 
-nets = {"transfer": TransferNet, "simple": Net, "resnet":ResNet, "grayscale": GrayNet}
+nets = {"transfer": TransferNet, "simple": Net, "resnet":ResNet, "grayscale": GrayNet, "layeredresnet": LayeredResNet}
 
 
-def get_net(net_name, num_classes):
+def get_net(net_name, num_classes,params_t):
     if net_name in nets:
-        return nets[net_name](num_classes)
+        return nets[net_name](num_classes,params_t)
     else:
         raise Exception("{} is not a recognized net structure".format(net_name))
 
